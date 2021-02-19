800e99ef58b3d64e5adb01978427413940bad80e
antonzaharia <zaharia_anton@yahoo.com>
2021-02-19 14:48:34 +0000

    questions app refactored using events

--- node-modules/questions.js
+++ node-modules/questions.js
@@ -5,8 +5,13 @@ const questions = [
   "Where are you going to do with node js? ",
 ];
 
-collectAnswers(questions, (answers) => {
+const answerEvents = collectAnswers(questions);
+
+answerEvents.on("answer", (answer) =>
+  console.log(`question answered: ${answer}`)
+);
+answerEvents.on("complete", (answers) => {
   console.log(" Thank you for your answers.");
   console.log(answers);
-  process.exit();
 });
+answerEvents.on("complete", () => process.exit());
