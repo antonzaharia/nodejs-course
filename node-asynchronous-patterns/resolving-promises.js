d1d53c16209fc18e1bff376fd87d1dd198d7f4d2
antonzaharia <zaharia_anton@yahoo.com>
2021-02-22 16:29:39 +0000

    node resolving promises

---
+++ node-asynchronous-patterns/resolving-promises.js
@@ -0,0 +1,13 @@
+const delay = (seconds) =>
+  new Promise((resolves, rejects) => {
+    setTimeout(() => {
+      resolves("the long delay has ended");
+    }, seconds * 1000);
+  });
+
+delay(1)
+  .then(console.log)
+  .then(() => 42)
+  .then((number) => console.log(number));
+
+console.log("end first tick");
