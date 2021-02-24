63e77d27b2560ec549766797059a9ef96b22431f
antonzaharia <zaharia_anton@yahoo.com>
2021-02-24 15:37:12 +0000

    node graphTasks()

--- node-asynchronous-patterns/concurrent-tasks.js
+++ node-asynchronous-patterns/concurrent-tasks.js
@@ -1,3 +1,5 @@
+const logUpdate = require("log-update");
+const toX = () => "X";
 const delay = (seconds) =>
   new Promise((resolves) => {
     setTimeout(resolves, seconds * 1000);
@@ -30,14 +32,27 @@ class PromiseQueue {
   get runAnother() {
     return this.running.length < this.concurrent && this.todo.length;
   }
+
+  graphTasks() {
+    const { todo, running, complete } = this;
+    logUpdate(`
+    
+    todo: [${todo.map(toX)}]
+    running: [${running.map(toX)}]
+    complete: [${complete.map(toX)}]
+
+    `);
+  }
   run() {
     while (this.runAnother) {
       const promise = this.todo.shift();
       promise.then(() => {
         this.complete.push(this.running.shift());
+        this.graphTasks();
         this.run();
       });
       this.running.push(promise);
+      this.graphTasks();
     }
   }
 }
