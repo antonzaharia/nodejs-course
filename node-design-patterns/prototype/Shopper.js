a5fc5d00d00bdcedd36949b225ae2fe112b03b7d
antonzaharia <zaharia_anton@yahoo.com>
2021-03-02 14:56:10 +0000

    node prototype pattern clone()

--- node-design-patterns/prototype/Shopper.js
+++ node-design-patterns/prototype/Shopper.js
@@ -1,19 +1,33 @@
 const logger = require("./logger");
 
 class Shopper {
-  constructor(name, money = 0) {
-    this.name = name;
-    this.money = money;
-    this.list = [];
-    logger.log(`New Shopper: ${name} has ${money} in their account.`);
+  constructor(name = "unnamed person", money = 0) {
+    this._name = name;
+    this._list = [];
+    logger.log(`New Shopper: ${name} has been created.`);
   }
-  shoppingList = () => {
-    return this.list;
-  };
-  addItemToList = (item) => {
-    this.list.push(item);
-    logger.log(`Item added: ${this.list}`);
-  };
+  set name(value) {
+    this._name = value;
+  }
+  get name() {
+    return this._name;
+  }
+  get shoppingList() {
+    return this._list.join(", ");
+  }
+  addItemToList(item) {
+    this._list.push(item);
+    logger.log(`Item added: ${this._list}`);
+  }
+  clone() {
+    const proto = Object.getPrototypeOf(this);
+    const cloned = Object.create(proto);
+
+    cloned._name = this._name;
+    cloned._list = [...this._list];
+
+    return cloned;
+  }
 }
 
 module.exports = Shopper;
