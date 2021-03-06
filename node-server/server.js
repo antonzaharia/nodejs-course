e572bf01cac7640c9d50df17e4d4a7423052ce9e
antonzaharia <zaharia_anton@yahoo.com>
2021-03-06 14:19:56 +0000

    node html template loaded with assets

--- node-server/server.js
+++ node-server/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 
 const port = 3000;
+app.use(express.static(path.join(__dirname, "./statics")));
 
 app.get("/", (request, response) => {
-  response.send("Hello Express :)");
+  response.sendFile(path.join(__dirname, "./statics/index.html"));
 });
 
 app.listen(port, () => {
