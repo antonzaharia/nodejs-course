450cc4be71e904e0b59dea8f9dd570d5d170f975
antonzaharia <zaharia_anton@yahoo.com>
2021-03-09 15:00:37 +0000

    node react mongodb react props validation

--- node-mongodb/src/index.js
+++ node-mongodb/src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const color = Math.random() > 0.5 ? "green" : "red";
+const App = (props) => {
+  return <h2 className="text-center">{props.headerMessage}</h2>;
+};
 
-ReactDOM.render(
-  <h2 style={{ color }} className="text-center">
-    Hello React with JSX!! {Math.random()}
-  </h2>,
-  document.getElementById("root")
-);
+App.propTypes = {
+  headerMessage: React.PropTypes.string,
+};
+App.defaultProps = {
+  headerMessage: "Hello!",
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
