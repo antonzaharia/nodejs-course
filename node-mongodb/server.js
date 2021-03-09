b39a4b3c5fc346441f9f212ce10534af482e1751
antonzaharia <zaharia_anton@yahoo.com>
2021-03-09 13:06:04 +0000

    node react mongodb EJS template

--- node-mongodb/server.js
+++ node-mongodb/server.js
@@ -1,20 +1,18 @@
 import config from "./config";
 import apiRouter from "./api";
 import express from "express";
+
 const server = express();
+server.set("view engine", "ejs");
 
 server.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.render("index", {
+    content: "Hello rendered in <em>EJS</em>",
+  });
 });
 
 server.use("/api", apiRouter);
 server.use(express.static("public"));
-
-// server.get("/about.html", (req, res) => {
-//   fs.readFile("./about.html", (err, data) => {
-//     res.send(data.toString());
-//   });
-// });
 
 server.listen(config.port, () => {
   console.log("Express listening on port ", config.port);
