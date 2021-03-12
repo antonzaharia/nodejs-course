3371cce99d383bfc06ca22b4bee80ee450f6e527
antonzaharia <zaharia_anton@yahoo.com>
2021-03-12 14:27:18 +0000

    node ract mongodb - navigating to a contest

--- node-mongodb/src/components/App.js
+++ node-mongodb/src/components/App.js
@@ -2,18 +2,25 @@ import React from "react";
 import Header from "./Header";
 import ContestList from "./ContestList";
 
+const pushState = (obj, url) => window.history.pushState(obj, "", url);
+
 class App extends React.Component {
   state = {
     contests: this.props.initialContest,
     pageHeader: "Naming Contests",
   };
-  componentDidMount() {}
+  fetchContest = (contestId) => {
+    pushState({ currentContestId: contestId }, `/contest/${contestId}`);
+  };
 
   render() {
     return (
       <div className="App">
         <Header message={this.state.pageHeader} />
-        <ContestList contests={this.state.contests} />
+        <ContestList
+          onContestClick={this.fetchContest}
+          contests={this.state.contests}
+        />
       </div>
     );
   }
--- node-mongodb/src/components/ContestList.js
+++ node-mongodb/src/components/ContestList.js
@@ -2,15 +2,20 @@ import React from "react";
 import ContestPreview from "./ContestPreview";
 import PropTypes from "prop-types";
 
-export default function ContestList({ contests }) {
+export default function ContestList({ contests, onContestClick }) {
   return (
     <div>
       {contests.map((contest) => (
-        <ContestPreview key={contest.id} {...contest} />
+        <ContestPreview
+          key={contest.id}
+          onClick={onContestClick}
+          {...contest}
+        />
       ))}
     </div>
   );
 }
 ContestList.propTypes = {
   contests: PropTypes.array,
+  onContestClick: PropTypes.func.isRequired,
 };
