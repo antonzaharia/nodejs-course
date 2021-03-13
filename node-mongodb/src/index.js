f7f54500dce3fe4a6c7f83e13940e58d2cdde549
antonzaharia <zaharia_anton@yahoo.com>
2021-03-13 15:31:32 +0000

    node ract mongodb - handling back button

--- node-mongodb/src/index.js
+++ node-mongodb/src/index.js
@@ -7,3 +7,6 @@ ReactDOM.render(
   <App initialData={window.initialData} />,
   document.getElementById("root")
 );
+setTimeout(function () {
+  ReactDOM.render(<h2>Clear!!</h2>, document.getElementById("root"));
+}, 5000);
