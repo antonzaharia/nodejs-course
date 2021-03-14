fc31d01a44193c26dda5a767e5c5a0da17744301
antonzaharia <zaharia_anton@yahoo.com>
2021-03-14 10:01:50 +0000

    node react mongodb db creation

--- node-mongodb/api/index.js
+++ node-mongodb/api/index.js
@@ -1,24 +1,43 @@
 import express from "express";
-import data from "../src/testData.json";
+import { MongoClient } from "mongodb";
+import assert from "assert";
+const config = {
+  mongodbURI: "mongodb://localhost:27017/test",
+};
+
+let mdb;
+MongoClient.connect(config.mongoDbUri, (err, db) => {
+  assert.strictEqual(null, err);
+  mdb = db;
+});
 
 const router = express.Router();
-const contests = data.contests.reduce((obj, contest) => {
-  obj[contest.id] = contest;
-  return obj;
-}, {});
 
 router.get("/contests", (req, res) => {
-  res.send({
-    contests: contests,
-  });
+  let contests = {};
+  mdb
+    .collection("contests")
+    .find({})
+    .project({
+      id: 1,
+      categoryName: 1,
+      contestName: 1,
+    })
+    .each((err, contest) => {
+      assert.strictEqual((null, err));
+      if (!contest) {
+        res.send({ contests });
+        return;
+      }
+      contests[contest.id] = contest;
+    });
 });
 router.get("/contests/:contestId", (req, res) => {
-  // req.params.contestId
-  let contest = contests[req.params.contestId];
-  contest.description =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-
-  res.send(contest);
+  mdb
+    .collection("contests")
+    .findOne({ id: Number(req.params.contestId) })
+    .then((contest) => res.send(contest))
+    .catch(console.error);
 });
 
 export default router;
